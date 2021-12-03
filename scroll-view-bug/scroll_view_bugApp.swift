547fafc619314e3a9171919859a2cc2acb8c2fd3
Patrick Dinger <121539+paxos@users.noreply.github.com>
2021-12-03 10:51:05 -1000

    Initial Commit

---
+++ scroll-view-bug/scroll_view_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  scroll_view_bugApp.swift
+//  scroll-view-bug
+//
+//  Created by Patrick Dinger on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct scroll_view_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
